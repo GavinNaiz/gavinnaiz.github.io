5b75f3b92b32d2b36aebe3534018512a2bbf9e03
GavinNaiz <gavtronone@gmail.com>
2017-11-07 20:25:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ img1.onclick = function(){
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
